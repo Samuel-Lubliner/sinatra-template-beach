8731f8bf8840f84b4c96b6fc3a12b5f50859ebec
Samuel Lubliner <74507133+Samuel-Lubliner@users.noreply.github.com>
2023-08-08 21:18:34 +0000

    beach rec list and about page

--- app.rb
+++ app.rb
@@ -1,9 +1,25 @@
-require "sinatra"
-require "sinatra/reloader"
+require 'sinatra'
+require 'http'
+require 'json'
 
-get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+get '/' do
+  erb :about
+end
+
+get '/about' do
+  erb :about
+end
+
+
+get '/prediction' do
+
+  current_date = Time.now.strftime('%Y-%m-%d')
+
+  response = HTTP.get("https://data.cityofchicago.org/resource/xvsz-3xcj.json?date=#{current_date}")
+
+  beaches_data = JSON.parse(response.to_s)
+
+  @beaches = beaches_data.sort_by { |beach| beach["predicted_level"].to_f }
+
+  erb :beach_prediction
 end
